6627fe2ea4637bd6999feff77ceb5bc460741a9f
Franek <betweter6@gmail.com>
2024-03-09 17:50:07 +0100

    dodano komentarz o celu wczyt.c

--- src/wczyt.c
+++ src/wczyt.c
@@ -1,5 +1,10 @@
 #include "wczyt.h"
 
 int wczyt(FILE* in, FILE* out, int verb, int dev){
+  /*
+   * Wczytać wymiary labiryntu
+   * Sprawdzić poprawność (punkty P i K, poza tym brak przerw w murze)
+  */
+
   return 0;
 }
