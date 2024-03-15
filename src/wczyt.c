222e21b1713543c7170e16fddbe8d885bad7d3bb
Franek <betweter6@gmail.com>
2024-03-15 09:16:35 +0100

    Dodano komentarz do wczyt.c

--- src/wczyt.c
+++ src/wczyt.c
@@ -4,6 +4,12 @@ int wczyt(FILE* in, FILE* out, int verb, int dev){
   /*
    * Wczytać wymiary labiryntu
    * Sprawdzić poprawność (punkty P i K, poza tym brak przerw w murze)
+   * Podzielić labirynt na kawałki o maksymalnych wymiarach około 128x128
+   * i zrobić folder tymczasowy i w nim zapisać te podziały.
+   * Ewentualnie rozpatrzyć też sytuację, że kawałek jest mniejszy i wtedy
+   * Można dodać w pierwszej linijce jego pliku jego wymiary
+   * (można też zawsze dla spokoju)
+   * Nazwać mapki można 1x1, 1x2 czy coś takiego żeby było wiadomo co z czym sąsiaduje. Takie mi rzeczy przyszły do głowy.
   */
 
   return 0;
